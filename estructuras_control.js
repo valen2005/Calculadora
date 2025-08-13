02f0779064429f2b06249e99f59b76e6fb15ed6e
Valentina <gavalentina365@gmail.com>
2025-08-12 19:33:06 -0500

    if, if else, if anidado

--- estructuras_control.js
+++ estructuras_control.js
@@ -13,3 +13,24 @@ let edad = 18;
  let password = "123456"; 
  if (password === "123456")
      { console.log("Acceso concedido"); } // Resultado: "Acceso concedido"
+
+ let edad2 = 12;
+  if (edad >= 14)
+     { console.log("Puedes votar"); } 
+  else { console.log("Aún no puedes votar"); } // Resultado: "Aún no puedes votar
+
+  let numero = 7;
+  if (numero % 2 === 0) 
+    { console.log("Es par"); }
+   else { console.log("Es impar"); } // Resultado: "Es impar"
+
+   let calificacion = 65;
+    if (calificacion >= 90)
+         { console.log("Excelente - A"); } 
+    else if (calificacion >= 80) 
+        { console.log("Muy bueno - B"); } 
+    else if (calificacion >= 70) 
+        { console.log("Bueno - C"); } 
+    else if (calificacion >= 60) 
+        { console.log("Suficiente - D"); } 
+    else { console.log("Insuficiente - F"); } // Resultado: "Muy bueno - B
