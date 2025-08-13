ebdf0020940d6b5384eb8ba4ee83b9a8304d8e6b
Valentina <gavalentina365@gmail.com>
2025-08-12 19:17:24 -0500

    archivo de estructuras de control

---
+++ estructuras_control.js
@@ -0,0 +1,15 @@
+let temperatura = 20; 
+if (temperatura >= 20) 
+    { console.log("Hace calor!"); } // Resultado: "Hace calor!"
+
+let edad = 18;
+ if (edad >= 18)
+     { console.log("Eres mayor de edad"); } // Resultado: "Eres mayor de edad" 
+ 
+ let dinero = 100;
+ let precio = 50; 
+ if (dinero >= precio) { console.log("Puedes comprar el producto"); } // Resultado: "Puedes comprar el producto"
+
+ let password = "123456"; 
+ if (password === "123456")
+     { console.log("Acceso concedido"); } // Resultado: "Acceso concedido"
